11d694ce723164141d5e8e5b09f97e6aeefe9608
gihorski <jag453@drexel.edu>
2016-02-20 23:14:10 -0500

    Created person.java class to hold first name, last name, and abbreviation or prefix of person.  Added changes to importer file to reflect update class.

--- src/storybook/importer/CharactersFromTextFileImporter.java
+++ src/storybook/importer/CharactersFromTextFileImporter.java
@@ -2,8 +2,11 @@ package storybook.importer;
 
 import java.io.File;
 import java.io.IOException;
+
 import javax.swing.JFileChooser;
+
 import org.apache.commons.io.FileUtils;
+
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.crf.*;
 import edu.stanford.nlp.io.IOUtils;
@@ -24,6 +27,8 @@ public class CharactersFromTextFileImporter{
 	 * @param parent
 	 * @param modal
 	 */
+		private person p; // person constructor object 
+	
 	public CharactersFromTextFileImporter(MainFrame m) {
 		mainFrame = m;
 	}
@@ -47,8 +52,8 @@ public class CharactersFromTextFileImporter{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// arraylist for names of people
 		ArrayList<String> people = new ArrayList<String>();
+		ArrayList<person> myperson = new ArrayList<person>(); // arraylist for names of people
 		// Make sure this file is in your eclipse root folder
 		AbstractSequenceClassifier<CoreLabel> classifier;
 		try {
@@ -65,16 +70,20 @@ public class CharactersFromTextFileImporter{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		int count = 0; // counter for fancy for loop
-		CharSequence prefixes = "Mr.","Dr.","Ms.","Miss.","Mrs.","mr","dr","ms","miss","mrs","uncle","aunt","mom","dad","sister","brother","cousin"); // character sequences of common prefixes
-		for(String name: people){ // iterates over peoples names
-			if(name.toLowerCase().contains(prefixes.toLowerCase())){	 // check if persons' name contains a prefix
-					String[] prename = name.split(" ",2); // split the prefix from the name
-					people.get(count) = prename[1]; // save the name to the people's arraylist
-				}
-			count++; // increment counter + 1
-			}
-		// end prefix loop
+		for(String ppl: people){ // iterate list names of people
+			p = new person(); // create new person object
+			String First_name = ""; // set first name empty
+			String Last_name = ""; // set last name empty
+			String Abbreviation = ""; // set abbreviation empty
+			Last_name =  ppl.substring(ppl.lastIndexOf(" ")+1); // remove first name and abbreviation from string
+			p.set_lastName(Last_name); // set person object last name
+			First_name = ppl.substring(0, ppl.lastIndexOf(' ')); // remove last name from string
+			p.set_firstName(First_name); // set person object first name
+			int x = ppl.indexOf(' '); // get index of first space
+			Abbreviation = ppl.substring(0, x); // remove first and last name from string
+			p.set_abrv(Abbreviation); // set person object abrreviation
+			myperson.add(p); // add person object to arraylist
+			}// end people loop
 		}
 	}
 }

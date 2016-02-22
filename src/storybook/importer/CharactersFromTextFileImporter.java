64bd19da6fd91e95bac16964712e4f7858023172
gihorski <jag453@drexel.edu>
2016-02-22 16:28:26 -0500

    We do not have to find prefixes.  Prefixes are a waste of time.  The proposed change never mentioned prefixes.  So this change removes checking for prefixes and sets the first name, last name, abbreviation, and gender like we are suppose to.  Next commit will set gender in person p object. Mixed results testing textfile with line breaks and names all on one line.  We need to come up with a delimiter to separate unqiue names.  Right now we are relying on the NLP parser to pick the person name skipping over unused names.

--- src/storybook/importer/CharactersFromTextFileImporter.java
+++ src/storybook/importer/CharactersFromTextFileImporter.java
@@ -2,11 +2,8 @@ package storybook.importer;
 
 import java.io.File;
 import java.io.IOException;
-
 import javax.swing.JFileChooser;
-
 import org.apache.commons.io.FileUtils;
-
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.crf.*;
 import edu.stanford.nlp.io.IOUtils;
@@ -14,7 +11,6 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
 import edu.stanford.nlp.util.Triple;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,19 +29,6 @@ public class CharactersFromTextFileImporter{
 	
 	public void importCharactersFromTextFile()
 	{
-		String[] prefixes = {"dr.", "dr", "doctor", "mr.", "mr", "mister", "ms.", "ms", "miss", "mrs.","mrs", "mistress", 
-				"hn.", "hn", "honorable", "the", "honorable", "his", "her", "honor", "fr", "fr.",
-			     "frau", "hr", "herr", "rv.", "rv", "rev.", "rev", "reverend", "reverend", "madam", "lord", "lady",
-			     "sir", "senior", "bishop", "rabbi", "holiness", "rebbe", "deacon", "eminence", "majesty", "consul",
-			     "vice", "president", "ambassador", "secretary", "undersecretary", "deputy", "inspector", "ins.",
-			     "detective", "det", "det.", "constable", "private", "pvt.", "pvt", "petty", "p.o.", "po", "first",
-			     "class", "p.f.c.", "pfc", "lcp.", "lcp", "corporal", "cpl.", "cpl", "colonel", "col", "col.",
-			     "capitain", "cpt.", "cpt", "ensign", "ens.", "ens", "lieutenant", "lt.", "lt", "ltc.", "ltc",
-			     "commander", "cmd.", "cmd", "cmdr", "rear", "radm", "r.adm.", "admiral", "adm.", "adm", "commodore",
-			     "cmd.", "cmd", "general", "gen", "gen.", "ltgen", "lt.gen.", "maj.gen.", "majgen.", "major", "maj.",
-			     "mjr", "maj", "seargent", "sgt.", "sgt", "chief", "cf.", "cf", "petty", "officer", "c.p.o.", "cpo",
-			     "master", "cmcpo", "fltmc", "formc", "mcpo", "mcpocg", "command", "fleet", "force", "miss.", 
-			     "professor","prof","uncle","aunt","dad","mom","sister","brother","cousin","grandfather","grandmother"};
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		int returnValue = fileChooser.showOpenDialog(mainFrame);
@@ -60,36 +43,36 @@ public class CharactersFromTextFileImporter{
 			e.printStackTrace();
 			System.out.println(e);
 		}
-		int num = 0;
-		String[] aline = lines.split(System.getProperty("line.separator")); // separate the string by lines
 		ArrayList<String> people = new ArrayList<String>(); // arraylist of people names
+		ArratList<Person> aperson = new ArrayList<Person>();
 		// Make sure this file is in your eclipse root folder
 		AbstractSequenceClassifier<CoreLabel> classifier;
-		for(String l: aline){ // read each line of file
 			try {
 				classifier = CRFClassifier.getClassifier("english.all.3class.distsim.crf.ser.gz");
 				List<Triple<String, Integer, Integer>> list = classifier.classifyToCharacterOffsets(l);
-				for(Triple<String, Integer, Integer> item : list){
-					if (item.first().equals("PERSON")){ // look at tag for person
-						for(String pre: prefixes){ // iterate through common prefixes
-		          			if (l.toLowerCase().indexOf(pre.toLowerCase()) != -1 ) { // find index of prefix in sentence
-		          				num = l.toLowerCase().indexOf(pre.toLowerCase()); // prefix found save index
-		          				people.add(l.substring(num, trip.third())); // add name to array with prefix at new index
-		          				break; // break for loop
-		          				} 
-		          			} // exit prefix for loop
-		          		if(num == 0){ // check if prefix not found 
-		          			people.add(l.substring(trip.second, trip.third())); // add name without prefix
-		          		}
-					}
-				}
+				  for (Triple<String, Integer, Integer> trip : list) {
+			          	if(trip.first().equals("PERSON")){
+			          		Person p = new Person();
+			          		String name = lines.substring(trip.second, trip.third());
+			          		name = name.replace("\n", " ").replace("\r", " ").replaceAll("\\s+", " ").trim();
+			          		String[] names = name.split(" ");
+			          		p.setFirstname(names[0]);
+			          		if(names.length>1){
+			          			p.setLastname(names[names.length -1 ]);
+			          			}
+			          		StringBuffer abbreviation = new StringBuffer(p.getFirstname().substring(0, 2));
+							if (names.length > 1) {
+								abbreviation.append(p.getLastname().substring(0,2));
+							}
+							p.setAbbreviation(abbreviation.toString());
+			          		}
+			          	}
+				  aperson.add(p);
 			} 
 			catch (ClassCastException | ClassNotFoundException | IOException e) {
 				e.printStackTrace();
 				System.out.println(e);
 			}
-		num = 0; // clear index
-		}
 		for(int c = 0; c < people.size(); c++){
 			System.out.println(c + ":" + people.get(c));
 	     }
